bc97c7d790f9410a2a85826741bffa711692b056
TruongToan <toan111xyz@gmail.com>
2023-08-15 15:34:57 +0700

    sua string? email thanh string email = string.empty;

--- Models/Domain/Employee.cs
+++ Models/Domain/Employee.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string? Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         public long Salary { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Department { get; set; } = string.Empty;
